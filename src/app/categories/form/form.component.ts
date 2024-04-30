e44d8d3f68223940c735d63bad05fa5769aa930f
mattyonder <js.mateussantos05@gmail.com>
2024-04-30 19:03:23 -0300

    Formulario de criação de categoria

--- src/app/categories/form/form.component.ts
+++ src/app/categories/form/form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'category-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule
+  ],
   templateUrl: './form.component.html',
   styles: ``
 })
 export class CategoryFormComponent {
+  private fb = inject(FormBuilder)
+  categoryForm = this.fb.group({
+    id: [null],
+    name: ['', Validators.required],
+    description: ['', Validators.required]
+
+  })
 
 }
